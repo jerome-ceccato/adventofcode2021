9a8f602325a39941f8bb76b849e73eb14caa4af0
IATGOF <jerome.ceccato@gmail.com>
2021-12-20 23:35:47 +0100

    Day 16

--- day16/day16.swift
+++ day16/day16.swift
@@ -8,6 +8,26 @@
 import Foundation
 
 final class Day16: AOCDay {
+    enum PacketOperator: Int {
+        case sum = 0
+        case product = 1
+        case min = 2
+        case max = 3
+        case gt = 5
+        case lt = 6
+        case eq = 7
+    }
+    
+    enum PacketType {
+        case number(n: Int)
+        case op(op: PacketOperator, subpackets: [Packet])
+    }
+            
+    struct Packet {
+        let version: Int
+        let type: PacketType
+    }
+    
     func parseInput(_ raw: String) -> String {
         return raw.trimmingCharacters(in: .whitespacesAndNewlines).map { char in
             let hex = Int(String(char), radix: 16)!
@@ -25,39 +45,87 @@ final class Day16: AOCDay {
     func consumeAsInt(bits: Int, input: inout String) -> Int {
         return Int(consume(bits: bits, input: &input), radix: 2)!
     }
+    
+    func consumeLiteralValue(input: inout String) -> Int {
+        var valueBits = ""
+        
+        while consume(bits: 1, input: &input) == "1" {
+            valueBits += consume(bits: 4, input: &input)
+        }
+        valueBits += consume(bits: 4, input: &input)
+        return Int(valueBits, radix: 2)!
+    }
    
-    func decode(input: inout String) -> Int {
+    func decode(input: inout String) -> Packet {
         let version = consumeAsInt(bits: 3, input: &input)
         let type = consumeAsInt(bits: 3, input: &input)
-        
-        var total = version
+
         if type == 4 {
-            while consume(bits: 5, input: &input).starts(with: "1") {}
-        } else {
+            return Packet(version: version, type: .number(n: consumeLiteralValue(input: &input)))
+        } else if let opType = PacketOperator(rawValue: type) {
+            var subpackets = [Packet]()
             if consumeAsInt(bits: 1, input: &input) == 0 {
                 let length = consumeAsInt(bits: 15, input: &input)
-                var subpackets = consume(bits: length, input: &input)
-                while !subpackets.isEmpty {
-                    total += decode(input: &subpackets)
+                var subInput = consume(bits: length, input: &input)
+                while !subInput.isEmpty {
+                    subpackets.append(decode(input: &subInput))
                 }
             } else {
                 let nSubpackets = consumeAsInt(bits: 11, input: &input)
                 for _ in 0 ..< nSubpackets {
-                    total += decode(input: &input)
+                    subpackets.append(decode(input: &input))
                 }
             }
+            return Packet(version: version, type: .op(op: opType, subpackets: subpackets))
         }
-        return total
+        fatalError()
     }
 
     func part1(rawInput: String) -> CustomStringConvertible {
         var input = parseInput(rawInput)
-        return decode(input: &input)
+        let packet = decode(input: &input)
+        
+        func versionSum(packet: Packet) -> Int {
+            switch packet.type {
+            case .op(_, let subpackets):
+                return packet.version + subpackets.map(versionSum(packet:)).reduce(0, +)
+            case .number:
+                return packet.version
+            }
+        }
+        
+        return versionSum(packet: packet)
     }
 
     func part2(rawInput: String) -> CustomStringConvertible {
-        // let input = parseInput(rawInput)
+        var input = parseInput(rawInput)
+        let packet = decode(input: &input)
         
-        return "Unimplemented"
+        func process(packet: Packet) -> Int {
+            switch packet.type {
+            case .number(let n):
+                return n
+            case .op(let op, let subpackets):
+                let subValues = subpackets.map(process(packet:))
+                switch op {
+                case .sum:
+                    return subValues.reduce(0, +)
+                case .product:
+                    return subValues.reduce(1, *)
+                case .min:
+                    return subValues.min()!
+                case .max:
+                    return subValues.max()!
+                case .gt:
+                    return subValues[0] > subValues[1] ? 1 : 0
+                case .lt:
+                    return subValues[0] < subValues[1] ? 1 : 0
+                case .eq:
+                    return subValues[0] == subValues[1] ? 1 : 0
+                }
+            }
+        }
+        
+        return process(packet: packet)
     }
 }
